449f8aac0e1b964b6fdc51be9ca69c22c74521ff
Mohammed Sahl <sahl.kably@gmail.com>
2020-08-06 19:49:29 -0400

    Add links

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,42 @@ import { IOptions } from "tsparticles/dist/Options/Interfaces/IOptions";
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { faEnvelope, faFilePdf } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 
 function App() {
     const cardStyle = {
-        width: '18em',
+        width: '30em',
+        height: '12em',
         margin: 'auto',
-        display: 'block',
     }
     return (
         <div className="App parent">
             <Particles options={particlesOptions as RecursivePartial<IOptions>} />
             <Card style={cardStyle} bg='Light'>
                 <Card.Body>
-                    <Card.Title size="2x"><b>Mohammed</b> Sahl</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">Computer Science '23, University of Waterloo</Card.Subtitle>
+                    <Card.Title style={{ fontSize: '2em' }}><b>Mohammed</b> Sahl</Card.Title>
+                    <Card.Subtitle  style={{ padding: '1em' }} className="mb-2 text-muted">
+                        Computer Science '23, University of Waterloo
+                    </Card.Subtitle>
                     <div className="links">
-                        <Card.Link style={{ 'color': 'inherit' }} href="https://www.github.com/mohammedsahl">
+                        <Card.Link style={{ 'color': 'inherit' }} href="https://www.github.com/mohammedsahl" target="_blank">
                             <FontAwesomeIcon icon={faGithub} size="3x" className="fa-button" />
                         </Card.Link>
 
-                        <Card.Link style={{ 'color': 'inherit' }} href="https://www.linkedin.com/in/mohammed-sahl">
+                        <Card.Link></Card.Link>
+
+                        <Card.Link style={{ 'color': 'inherit' }} href="https://www.linkedin.com/in/mohammed-sahl" target="_blank">
                             <FontAwesomeIcon icon={faLinkedinIn} size="3x" className="fa-button" />
                         </Card.Link>
+
+                        <Card.Link></Card.Link>
+
+                        <Card.Link style={{ 'color': 'inherit' }} href="/resume.pdf"  download="resume.pdf">
+                                <FontAwesomeIcon icon={faFilePdf} size="3x" className="fa-button" />
+                        </Card.Link>
+
+                        <Card.Link></Card.Link>
 
                         <Card.Link style={{ 'color': 'inherit' }} href="mailto:mskably@uwaterloo.com">
                             <FontAwesomeIcon icon={faEnvelope} size="3x" className="fa-button" />
