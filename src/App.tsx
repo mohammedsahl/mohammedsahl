dc0737c3f107582a1b2f1de109f0f22e37133ce8
Mohammed Sahl <sahl.kably@gmail.com>
2020-10-02 13:55:46 -0400

    Update comments

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import { faEnvelope, faFilePdf } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
-    // Define card dimensions
     const cardStyle = {
         width: '30em',
         height: '12em',
